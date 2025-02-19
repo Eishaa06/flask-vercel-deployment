1c3b8e021a5104fd2ea4cbbb1fc7e725d1927f67
Eishaa06 <eishaharoon4@gmail.com>
2025-02-19 22:28:30 +0500

    hellooo

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "App is running!"
+    return "My name is Hassaanullah and I am a Data Scientist!"
 
 if __name__ == "__main__":
     app.run()
